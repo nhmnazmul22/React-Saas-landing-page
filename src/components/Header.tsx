5910492bee1d3add6fb98eef31cae306b80830a5
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-03-17 13:07:10 +0600

    Mobile Header done and themeing done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,22 +10,39 @@ import { Button } from '@/components/ui/button';
 import Logo from './Logo';
 
 /**
+ * Constants
+ */
+import { navMenu } from '@/constants';
+
+/**
  * Assets
  */
 import { Menu } from 'lucide-react';
+import MobileMenu from './MobileMenu';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 const Header = () => {
   return (
-    <header className=''>
-      <div className='container'>
+    <header className='h-16 grid grid-cols-1 items-center md:h-20 lg:24'>
+      <div className='container flex justify-between'>
         <Logo variant='icon' />
-        <Button
-          variant='outline'
-          size='icon'
-          className='lg:hidden'
-        >
-          <Menu />
-        </Button>
+        <Popover>
+          <PopoverTrigger>
+            <Button
+              variant='outline'
+              size='icon'
+              className='lg:hidden cursor-pointer'
+            >
+              <Menu />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            align='end'
+            className='bg-background/50 backdrop-blur-3xl border-foreground/5 border-x-0 border-b-0 rounded-lg overflow-hidden'
+          >
+            <MobileMenu navMenu={navMenu} />
+          </PopoverContent>
+        </Popover>
       </div>
     </header>
   );
